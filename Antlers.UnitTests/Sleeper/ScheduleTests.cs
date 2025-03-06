db7e8c486d57f2cea19195e802f2f15948e27d79
Tyler Evers <510808@corp.caseys.com>
2025-03-06 15:59:45 -0600

    Added Schedule and StatsFakers along with initial Schedule unit tests

---
+++ Antlers.UnitTests/Sleeper/ScheduleTests.cs
@@ -0,0 +1,28 @@
+﻿using Antlers.UnitTests.Sleeper.Mocks;
+using Newtonsoft.Json;
+
+namespace Antlers.UnitTests.Sleeper
+{
+    public class ScheduleTests
+    {
+        [Fact]
+        public void Schedule_Serialization_ReturnsCorrectJson()
+        {
+            // Arrange
+            var validSchedule = new ScheduleFaker().GenerateValidSchedule();
+            
+            // Act
+            var json = JsonConvert.SerializeObject(validSchedule);
+            
+            // Assert
+            string expectedJson = $"{{\"game_id\":\"{validSchedule.GameId}\"," +
+                                  $"\"week\":{validSchedule.Week}," +
+                                  $"\"date\":\"{validSchedule.Date}\"," +
+                                  $"\"home\":\"{validSchedule.Home}\"," +
+                                  $"\"away\":\"{validSchedule.Away}\"," +
+                                  $"\"status\":\"{validSchedule.Status}\"}}";
+
+            Assert.Equal(expectedJson, json);
+        }
+    }
+}
